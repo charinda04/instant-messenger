6fac96ed53c28b51a3155fd64a8883f5c7309288
charinda04 <charinda04@gmail.com>
2018-05-12 08:38:42 +0530

    gui step 1

--- src/Server.java
+++ src/Server.java
@@ -12,5 +12,25 @@ public class Server extends JFrame{
     private ServerSocket server;
     private Socket connection;
 
+    //constructor
+    public Server(){
+        super("chari's instant messenger");
+        userText = new JTextField();
+        userText.setEditable(false);
+        userText.addActionListener(
+                new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent actionEvent) {
+                        sendMessage(actionEvent.getActionCommand());
+                        userText.setText("");
+                    }
+                }
+        );
+        add(userText, BorderLayout.NORTH);
+        chatWindow = new JTextArea();
+        add(new JScrollPane(chatWindow));
+        setSize(300,150);
+        setVisible(true);
+    }
 
 }
